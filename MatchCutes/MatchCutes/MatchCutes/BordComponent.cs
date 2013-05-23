ae0f4b0c8b2b5776154835d7a859629148b735d2
Kari Helene Bekkelund <Kari.hdb@hotmail.com>
2013-05-23 16:40:53 +0200

    hgjhf

---
+++ MatchCutes/MatchCutes/MatchCutes/BordComponent.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace MatchCutes
+{
+
+    public class BordComponent : Microsoft.Xna.Framework.GameComponent
+    {
+        public BordComponent(Game game)
+            : base(game)
+        {
+            // TODO: Construct any child components here
+        }
+
+        public override void Initialize()
+        {
+            // TODO: Add your initialization code here
+
+            base.Initialize();
+        }
+
+
+        public override void Update(GameTime gameTime)
+        {
+            // TODO: Add your update code here
+
+            base.Update(gameTime);
+        }
+    }
+}
---
+++ MatchCutes/MatchCutes/MatchCutes/TextComponent1.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace MatchCutes
+{
+    /// <summary>
+    /// This is a game component that implements IUpdateable.
+    /// </summary>
+    public class TextComponent1 : Microsoft.Xna.Framework.GameComponent
+    {
+        public TextComponent1(Game game)
+            : base(game)
+        {
+            // TODO: Construct any child components here
+        }
+
+        public override void Initialize()
+        {
+            // TODO: Add your initialization code here
+
+            base.Initialize();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            // TODO: Add your update code here
+
+            base.Update(gameTime);
+        }
+    }
+}
